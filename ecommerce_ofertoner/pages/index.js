9b8bb0bcaa73dabcb30e46d55c1428a1fa54f0a5
leyonel <leonellayonel@gmail.com>
2022-10-23 11:29:54 -0600

    GetServerSideProps from Next.js

--- ecommerce_ofertoner/pages/index.js
+++ ecommerce_ofertoner/pages/index.js
@@ -1,10 +1,9 @@
 import React from 'react'
-
+import { client } from '../lib/client';
 import { Product, FooterBanner, HomeBanner } from '../components'
 
-const Home = () => {
-  return (
-    <>
+const Home = () => (
+    <div>
        <HomeBanner />
 
         <div className='products-heading'>
@@ -17,8 +16,18 @@ const Home = () => {
         </div>
 
        <FooterBanner />
-    </>
-  )
-}
+    </div>
+  );
 
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: {products, bannerData}
+  }
+}
 export default Home

ce2818a3696299d4afff07c859f65ea032662261
leyonel <leonellayonel@gmail.com>
2022-10-30 21:51:29 -0700

    footer container

--- ecommerce_ofertoner/components/Layout.jsx
+++ ecommerce_ofertoner/components/Layout.jsx
@@ -5,24 +5,20 @@ import Footer from './Footer';
 
 const Layout = ({children}) => {
   return (
-    <div className='layout'>
-      <Head>
-        <title>JS OFERTONER</title>
-      </Head>
-
-      <header>
-      
-        <Navbar/>
-       </header>
-
-       <main className='main-container'>
-            {children}
-       </main>
-
-       <footer>
-          <Footer/>
-       </footer>
-    </div>
+    <div className="layout">
+    <Head>
+      <title>JS Mastery Store</title>
+    </Head>
+    <header>
+      <Navbar />
+    </header>
+    <main className="main-container">
+      {children}
+    </main>
+    <footer>
+      <Footer />
+    </footer>
+  </div>
   )
 }
 

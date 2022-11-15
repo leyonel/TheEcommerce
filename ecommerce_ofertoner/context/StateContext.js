679e3d1de7f91a5db85701746fa5bc9b6e565c60
Leyonel <leonellayonel@gmail.com>
2022-11-15 13:21:31 -0700

    fix stateContext

--- ecommerce_ofertoner/context/StateContext.js
+++ ecommerce_ofertoner/context/StateContext.js
@@ -1,15 +1,14 @@
 import React, { createContext, useContext, useState, useEffect} from 'react';
-import toast, { Toast } from 'react-hot-toast';
+import { toast } from 'react-hot-toast';
 
 const Context = createContext();
 
 
 
-export const StatrContext = ({children}) => {
+export const StateContext = ({children}) => {
     const [showCart, setShowCart] = useState(false);
-
     const [cartItems, setCartItems] = useState([]);
-    const [totalPrice, setTotalPrice] = useState();
+    const [totalPrice, setTotalPrice] = useState(0);
     const [totalQuantities, setTotalQuantities] = useState(0);
     const [qty, setQty] = useState(1);
 
@@ -19,45 +18,47 @@ export const StatrContext = ({children}) => {
         setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
         setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
 
-        if (checkProductInCart) {
-            const updateCartItems = cartItems.map((cartProduct) => {
-                if (cartProduct._id === product._id) return {
-                    ...cartProduct,
-                    quantity: cartProduct.quantity + quantity
-                }
+        if(checkProductInCart) {
+            const updatedCartItems = cartItems.map((cartProduct) => {
+              if(cartProduct._id === product._id) return {
+                ...cartProduct,
+                quantity: cartProduct.quantity + quantity
+              }
             })
 
-            setCartItems(updateCartItems);
+            setCartItems(updatedCartItems);
           
         } else {
             product.quantity = quantity;
             
-            setCartItems([...cartItems, {...product}]);
-        }
-        toast.success(`${qty} ${product.name} Agregado al Carrito`);
+            setCartItems([...cartItems, { ...product }]);
+          }
+      
+          toast.success(`${qty} ${product.name} Agregar al carrito.`);
     }
 
     const incQty = () => {
         setQty((prevQty) => prevQty + 1);
-    }
-    const decQty = () => {
+      }
+      const decQty = () => {
         setQty((prevQty) => {
-            if (prevQty - 1 < 1) return 1;
-           return prevQty - 1});
-    }
-
+          if(prevQty - 1 < 1) return 1;
+         
+          return prevQty - 1;
+        });
+      }
     return (
         <Context.Provider
             value= {{
                 showCart,
-                setShowCart,
-                cartItems,
-                totalPrice,
-                totalQuantities,
-                qty,
-                incQty,
-                decQty,
-                onAdd
+        setShowCart,
+        cartItems,
+        totalPrice,
+        totalQuantities,
+        qty,
+        incQty,
+        decQty,
+        onAdd,
             }}
         >
             {children}

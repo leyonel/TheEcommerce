b4572a7e908dbf294802d972d454d50a368a1ef1
leyonel <leonellayonel@gmail.com>
2022-11-17 23:22:19 -0700

    Agregnando estilos

--- ecommerce_ofertoner/sanity_ofertoner/schemas/schema.js
+++ ecommerce_ofertoner/sanity_ofertoner/schemas/schema.js
@@ -4,6 +4,7 @@ import schemaTypes from 'all:part:@sanity/base/schema-type';
 import product from './product';
 import banner from './banner';
 
+
 export default createSchema({
  
   name: 'default',
